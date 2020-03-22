764eb2f887dd01fa81495ac7c58aaa7788a7a9e6
Max van Oord <max.van.oord@gmail.com>
2020-03-22 13:29:43 +0100

    login pagina's toegevoegd

---
+++ AndroidAppDeNachtwacht/app/src/main/java/com/example/androidappnachtwacht/CreateAccount.java
@@ -0,0 +1,16 @@
+package com.example.androidappnachtwacht;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class CreateAccount extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createaccount_activity);
+
+    }
+
+}

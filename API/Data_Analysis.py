3ae707a264b6dbeb9c52dbace51f9a09be670327
Max van Oord <max.van.oord@gmail.com>
2020-05-19 14:20:05 +0200

    Begin gemaakt aan data analyse functie

--- API/Data_Analysis.py
+++ API/Data_Analysis.py
@@ -1,11 +1,30 @@
 
 def analyseData( data ):
-    # input is a list of datapoints[time, heartrate]
+    # input is a list of datapoints[time, heartrate] of n length ordered with newest first
 
-    #TODO: Algorithm to detect a nightmare...
+    sensitivity = 1.3
+    durability = 3
 
-    wakeUp = False
+    total = 0
+    count = 0
+    alerter = 0
+    while count < len(data):
+        # Calculating base values
+        heartRate = int(data[count])
+        total += int(data[count])
+        count += 1
+        avg = total / count
 
-    return wakeUp
+        # print(heartRate, avg, count)
 
+        if heartRate > (avg * sensitivity):
+            alerter += 1
+        else:
+            alerter = 0
 
+        # When a alerting heartbeat is measured over mentioned durability a person is in a dream
+        if alerter >= durability:
+            print(count)
+            return True
+
+    return False
--- API/RestAPI_DeNachtwacht.py
+++ API/RestAPI_DeNachtwacht.py
@@ -41,7 +41,7 @@ app = Flask(__name__)
 
 
 @app.route( '/user', methods = ["GET", "POST"] )
-def registerUser():
+def user():
 
     # GET requests are used to retreive al information about a user [args -> 'uid']
     if request.method == "GET":
@@ -66,7 +66,7 @@ def registerUser():
         # making a csv file to store incomming sleep data on
         csvFileName = username + '_' + str(randint(1000, 9999)) + '_sleepData.csv'
 
-        with open(csvFileName, 'w', newline='') as file:
+        with open("DataStorage\\" + csvFileName, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['time', 'heartrate'])
 
@@ -117,23 +117,20 @@ def sleepData():
             for row in data:
                 writer.writerow([row[0], row[1]])
 
-        # Analyzing new data to check if a 'wakeUp' response is needed
-        with open(csvUser, 'r') as file:
-            csvIterator = csv.reader(file, delimiter=',')
-            next(csvIterator)
-            dataPoints = list(csvIterator)[:100]
-
-        heartrates = []
-        for datapoint in dataPoints:
-            heartrates.append(datapoint[1])
-        dataForAnalysis = list(reversed(heartrates))
-
-        analyseResult = da.analyseData(dataForAnalysis)
+        analyseResult = analyse(csvUser)
 
         res = json.dumps({ "succes" : True , "wakeup" : analyseResult})
 
         return res
 
+
+@app.route('/checkforwakeup', methods = ["GET"])
+def checkForWakeUp():
+    user = request.args.get('uid', None)
+
+    res = analyse(getCsvForUser(user))
+
+    return json.dumps({"wakeup" : res})
 
 
 # END:    http requests     #
@@ -146,8 +143,24 @@ def getCsvForUser(user):
     # returns the path to the user's csv file
 
     cursor.execute("""select csvfilename from "Users" where uid = %s""", (user,))
-    return str(cursor.fetchone()[0])
+    path = "DataStorage\\" + str(cursor.fetchone()[0])
+    return path
 
+def analyse(givenCsv):
+        with open(givenCsv, 'r') as file:
+            csvIterator = csv.reader(file, delimiter=',')
+            next(csvIterator)
+            dataPoints = list(csvIterator)
+            if len(dataPoints) > 100:
+                dataPoints = dataPoints[:100]
+
+        heartrates = []
+        for datapoint in dataPoints:
+            heartrates.append(datapoint[1])
+
+        analyseResult = da.analyseData(heartrates)
+
+        return analyseResult
 
 # END:   global funcs       #
 

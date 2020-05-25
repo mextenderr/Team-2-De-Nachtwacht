eeac2a402b33a72bf9c96ca3275997b72c09e07f
rverwayen <47591601+rverwayen@users.noreply.github.com>
2020-05-25 11:54:43 +0200

    alarm geluid geimplementeerd
    .

--- flutterApp/flutter_app/lib/alarm.dart
+++ flutterApp/flutter_app/lib/alarm.dart
@@ -1,6 +1,17 @@
+//import 'dart:html';
+//import 'dart:typed_data';
 import 'package:flutter/material.dart';
+//import 'dart:async';
+//import 'dart:io';
+//import 'package:flutter/services.dart';
+//import 'package:path_provider/path_provider.dart';
+import 'package:audioplayers/audio_cache.dart';
+//import 'package:audioplayers/audioplayers.dart';
 
 class Alarm extends StatefulWidget{
+      
+  
+  
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -10,13 +21,36 @@ class Alarm extends StatefulWidget{
 }
 
 class AlarmState extends State{
+  static AudioCache player = new AudioCache();
+
+ 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+     
     return Scaffold(
       appBar: AppBar(
         title:  Text("alarm"),
       ),
+      body: Center(
+        child : Column(
+                  children<Widget>:[
+                  FloatingActionButton(
+                  onPressed:() {
+                  const alarmAudioPath = "alarmsound1.mp3";
+                  player.play(alarmAudioPath);
+                  },
+                  Text(
+                  'Click for sound',
+                  textAlign: TextAlign.center,
+          ),
+          ),]
+        ),
+        
+        
+        
+      )
+      
     );
   }
   

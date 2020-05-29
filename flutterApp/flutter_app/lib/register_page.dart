d659997c722ae879e76f6bf8dbaa03125c82a1e2
Joeri <joerihuigsloot@icloud.com>
2020-05-29 15:10:14 +0200

    register page toegevoegd
    problemen met het runnen van de emulator. kijken of het in master lukt

--- flutterApp/flutter_app/lib/register_page.dart
+++ flutterApp/flutter_app/lib/register_page.dart
@@ -2,21 +2,20 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_app/profile.dart';
-import 'package:flutter_app/register_page.dart';
+import 'package:flutter_app/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
-class LoginPage extends StatefulWidget {
+class RegisterPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
 
   bool _isLoading = false;
 
-  @override
+@override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
     return Scaffold(
@@ -36,27 +35,28 @@ class _LoginPageState extends State<LoginPage> {
             headerSection(),
             textSection(),
             buttonSection(),
-            buttonRegisterSection(),
           ],
         ),
       ),
      );
   }
 
-  signIn(String username, String password) async {
+  signUp(String username, String age, String name, String password) async {
     Map data = {
       'username': username,
+      'age': age,
+      'name': name,
       'password': password
     };
     var jsonData = null;
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    var response = await http.post("http://127.0.0.1:8000/login", body: data);
+    var response = await http.post("http://127.0.0.1:5001/register", body: data);
     if (response.statusCode == 200) {
       jsonData = json.decode(response.body);
       setState(() {
         _isLoading = false;
         sharedPreferences.setString("token", jsonData['token']);
-        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:  (BuildContext context) => MainPage()), (Route <dynamic> route)  => false);
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:  (BuildContext context) => MyHomePage()), (Route <dynamic> route)  => false); //MyHomePage moet nieuwe pagina worden
       });
     }
     else {
@@ -75,32 +75,13 @@ class _LoginPageState extends State<LoginPage> {
           setState(() {
             _isLoading = true;
           });
-          signIn(usernameController.text, passwordController.text);
+          signUp(usernameController.text, ageController.text, nameController.text, passwordController.text);
         },
         color: Colors.purple,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(5.0),
           ),
-        child: Text("Sign In", style: TextStyle(color: Colors.white70)),
-      ),
-    );
-  }
-
-    Container buttonRegisterSection(){
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 40.0,
-      margin: EdgeInsets.only(top: 30.0),
-      padding: EdgeInsets.symmetric(horizontal: 30.0),
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()),);
-        },
-        color: Colors.purple,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5.0),
-          ),
-        child: Text("No account? Sign up!", style: TextStyle(color: Colors.white70)),
+        child: Text("Sign Up", style: TextStyle(color: Colors.white70)),
       ),
     );
   }
@@ -113,6 +94,10 @@ class _LoginPageState extends State<LoginPage> {
         children: <Widget> [
           txtUsername("Username", Icons.person),
           SizedBox(height: 30.0),
+          txtAge("Age", Icons.face),
+          SizedBox(height: 30.0),
+          txtName("Name", Icons.person),
+          SizedBox(height: 30.0),
           txtPassword("Password", Icons.lock),
         ],
       ),
@@ -120,12 +105,40 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   TextEditingController usernameController = new TextEditingController();
+  TextEditingController ageController = new TextEditingController();
+  TextEditingController nameController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
   TextFormField txtUsername(String title, IconData icon){
     return TextFormField(
       controller: usernameController,
       obscureText: title == "Username" ? false : true,
+      style: TextStyle(color: Colors.white70),
+      decoration: InputDecoration(
+        hintText: title,
+        hintStyle: TextStyle(color: Colors.white70),
+        icon: Icon(icon)
+      ),
+    );
+  }
+
+    TextFormField txtAge(String title, IconData icon){
+    return TextFormField(
+      controller: ageController,
+      obscureText: title == "Age" ? false : true,
+      style: TextStyle(color: Colors.white70),
+      decoration: InputDecoration(
+        hintText: title,
+        hintStyle: TextStyle(color: Colors.white70),
+        icon: Icon(icon)
+      ),
+    );
+  }
+
+  TextFormField txtName(String title, IconData icon){
+    return TextFormField(
+      controller: nameController,
+      obscureText: title == "Name" ? false : true,
       style: TextStyle(color: Colors.white70),
       decoration: InputDecoration(
         hintText: title,

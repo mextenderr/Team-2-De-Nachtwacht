061f3714f4f5508e9b074f7de859d1a54992fca7
Jawdat-Hamdan <47349336+Jawdat-Hamdan@users.noreply.github.com>
2020-06-03 15:35:40 +0200

    v7
    v7

--- flutterApp/flutter_app/lib/chart.dart
+++ flutterApp/flutter_app/lib/chart.dart
@@ -1,117 +1,89 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'dart:async' show Future;
-import 'package:csv/csv.dart';
-import 'package:bezier_chart/bezier_chart.dart';
-import 'package:flutter/services.dart' show rootBundle;
-// EXCLUDE_FROM_GALLERY_DOCS_END
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+//import 'package:fl_chart/fl_chart.dart';
+//import 'package:bezier_chart/bezier_chart.dart';
+import 'package:fcharts/fcharts.dart';
+
 import 'package:flutter/material.dart';
 
-class Chart extends StatefulWidget{
+class Chart extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     // TODO: implement createState
     return ChartState();
   }
-
-
 }
-
 
 class ChartState extends State {
   @override
-//  Widget build(BuildContext context) {
-//
-//    // TODO: implement build
-//    return Scaffold(
-//
-//      appBar: AppBar(
-//        title:  Text("chart")),
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initData();
+  }
+
+  List data1 = new List();
+  void initData() async {
+    data1 = await getData();
+    setState(() {
+      data1 = data1;
+
+    });
+  }
+
+  List <List<String>> value(){
+    final List <List<String>> valuee = [];
+    for (int i = 0; i < 5; i++) {
+     valuee.add([data1[i]["id"].toString(), data1[i]["name"]]);
+    }
+    return valuee;
+  }
 
 
-//      body: new Material(
-//        color: Colors.blue,
-//
-//        child: Center(
-//          child:Text("Hello"),
-//
-//      ),
-//    )
 
-  Widget sample2(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.red,
-        height: MediaQuery
-            .of(context)
-            .size
-            .height / 2,
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
-        child: BezierChart(
-          bezierChartScale: BezierChartScale.CUSTOM,
-          xAxisCustomValues: const [0, 3, 10, 15, 20, 25, 30, 35],
-          series: const [
-            BezierLine(
-              label: "Custom 1",
-              data: const [
-                DataPoint<double>(value: 10, xAxis: 0),
-                DataPoint<double>(value: 130, xAxis: 5),
-                DataPoint<double>(value: 50, xAxis: 10),
-                DataPoint<double>(value: 150, xAxis: 15),
-                DataPoint<double>(value: 75, xAxis: 20),
-                DataPoint<double>(value: 0, xAxis: 25),
-                DataPoint<double>(value: 5, xAxis: 30),
-                DataPoint<double>(value: 45, xAxis: 35),
-              ],
-            ),
-            BezierLine(
-              lineColor: Colors.blue,
-              lineStrokeWidth: 2.0,
-              label: "Custom 2",
-              data: const [
-                DataPoint<double>(value: 5, xAxis: 0),
-                DataPoint<double>(value: 50, xAxis: 5),
-                DataPoint<double>(value: 30, xAxis: 10),
-                DataPoint<double>(value: 30, xAxis: 15),
-                DataPoint<double>(value: 50, xAxis: 20),
-                DataPoint<double>(value: 40, xAxis: 25),
-                DataPoint<double>(value: 10, xAxis: 30),
-                DataPoint<double>(value: 30, xAxis: 35),
-              ],
-            ),
-            BezierLine(
-              lineColor: Colors.black,
-              lineStrokeWidth: 2.0,
-              label: "Custom 3",
-              data: const [
-                DataPoint<double>(value: 5, xAxis: 0),
-                DataPoint<double>(value: 10, xAxis: 5),
-                DataPoint<double>(value: 35, xAxis: 10),
-                DataPoint<double>(value: 40, xAxis: 15),
-                DataPoint<double>(value: 40, xAxis: 20),
-                DataPoint<double>(value: 40, xAxis: 25),
-                DataPoint<double>(value: 9, xAxis: 30),
-                DataPoint<double>(value: 11, xAxis: 35),
-              ],
-            ),
-          ],
-          config: BezierChartConfig(
-            verticalIndicatorStrokeWidth: 2.0,
-            verticalIndicatorColor: Colors.black12,
-            showVerticalIndicator: true,
-            contentWidth: MediaQuery
-                .of(context)
-                .size
-                .width * 2,
-            backgroundColor: Colors.red,
-          ),
+
+// X value -> Y value
+  final myData = [
+    ["x", "1"],
+    ["B", "❓"],
+    ["C", "✖"],
+    ["D", "❓"],
+    ["E", "✖"],
+    ["F", "✖"],
+    ["G", "✔"],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: LineChart(
+      lines: [
+        new Line<List<String>, String, String>(
+          data: value(),
+//          [
+//            [data1[1]["id"].toString(), "1"],
+//            ["B", "❓"],
+//            ["C", "✖"],
+//            ["D", "❓"],
+//            ["E", "✖"],
+//            ["F", "✖"],
+//            ["G", "✔"],
+//          ],
+          xFn: (datum) => datum[0],
+          yFn: (datum) => datum[1],
         ),
-      ),
-    );
+      ],
+      chartPadding: new EdgeInsets.fromLTRB(30.0, 10.0, 10.0, 30.0),
+    ));
+  }
+
+  Future<List> getData() async {
+    // String url = "http://www.localhost/projectd/slaapdata1";
+    String url = "https://jsonplaceholder.typicode.com/comments";
+    http.Response r = await http.get(url);
+    List data = json.decode(r.body);
+    return data;
   }
 }

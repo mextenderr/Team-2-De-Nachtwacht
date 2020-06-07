2834894d6478602c391a662840575940b726c8c8
Max van Oord <max.van.oord@gmail.com>
2020-06-07 21:47:41 +0200

    Oude android app verwijderd

--- AndroidAppDeNachtwacht/app/src/main/java/com/example/androidappnachtwacht/SerialListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.androidappnachtwacht;
-
-public interface SerialListener {
-    void onSerialRead(byte[] data);
-    void onConnectingFailed();
-    void onDisconnectingFailed();
-    void onConnectionSucces();
-    void test(String test);
-}
